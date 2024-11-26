ab3d1655b63fb56833b86b71cbeb701fdca667cf
rispele <rispelemain@gmail.com>
2024-11-27 00:50:55 +0500

    feat: some sugar for factory

--- Rdx/Objects/RdxObjectFactory.cs
+++ Rdx/Objects/RdxObjectFactory.cs
@@ -2,6 +2,7 @@ namespace Rdx.Objects;
 
 public class RdxObjectFactory
 {
+    private const long InitialVersion = 0;
     private readonly IReplicaIdProvider replicaIdProvider;
 
     public RdxObjectFactory(IReplicaIdProvider replicaIdProvider)
@@ -13,20 +14,42 @@ public class RdxObjectFactory
     {
         return new RdxValue<T>(value, replicaId, version, replicaIdProvider.GetReplicaId());
     }
-    
-    
+
+    public RdxValue<T> NewValue<T>(T value)
+    {
+        var replicaId = replicaIdProvider.GetReplicaId();
+        return new RdxValue<T>(value, replicaId, InitialVersion, replicaId);
+    }
+
     public RdxTuple<T1, T2> Tuple<T1, T2>(T1 first, T2 second, long replicaId, long version)
-        where T1: RdxObject
-        where T2: RdxObject
+        where T1 : RdxObject
+        where T2 : RdxObject
     {
         return new RdxTuple<T1, T2>(first, second, replicaId, version, replicaIdProvider.GetReplicaId());
     }
     
+    public RdxTuple<T1, T2> NewTuple<T1, T2>(T1 first, T2 second)
+        where T1 : RdxObject
+        where T2 : RdxObject
+    {
+        var replicaId = replicaIdProvider.GetReplicaId();
+        return new RdxTuple<T1, T2>(first, second, replicaId, InitialVersion, replicaId);
+    }
+
     public RdxTriple<T1, T2, T3> Triple<T1, T2, T3>(T1 first, T2 second, T3 third, long replicaId, long version)
-        where T1: RdxObject
-        where T2: RdxObject
-        where T3: RdxObject
+        where T1 : RdxObject
+        where T2 : RdxObject
+        where T3 : RdxObject
     {
         return new RdxTriple<T1, T2, T3>(first, second, third, replicaId, version, replicaIdProvider.GetReplicaId());
+    }
+    
+    public RdxTriple<T1, T2, T3> NewTriple<T1, T2, T3>(T1 first, T2 second, T3 third)
+        where T1 : RdxObject
+        where T2 : RdxObject
+        where T3 : RdxObject
+    {
+        var replicaId = replicaIdProvider.GetReplicaId();
+        return new RdxTriple<T1, T2, T3>(first, second, third, replicaId, InitialVersion, replicaId);
     }
 }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -20,26 +20,20 @@ static class Program
         var buffer = new RdxBuffer(1024);
         var factory = new RdxObjectFactory(new ConstIdProvider());
 
+
+        var t1 = factory.NewTuple(factory.NewValue(1), factory.NewValue(2));
+
+        var t2 = factory.NewTriple(
+            factory.NewValue(1),
+            factory.NewValue(1),
+            factory.NewValue(3));
+        
         var id = Random.Shared.NextInt64();
-        
-        var t1 = factory.Tuple(
-            factory.Value(1, id, 1),
-            factory.Value(2, id, 1),
-            Random.Shared.NextInt64(),
-            version: 1);
-        
-        var t2 = factory.Triple(
-            factory.Value(1, id, 1),
-            factory.Value(1, id, 1),
-            factory.Value(3, id, 1),
-            Random.Shared.NextInt64(),
-            version: 2);
-        
         var t3 = factory.Triple(
             factory.Value(1, id, 1),
             factory.Value(4, id, 1),
             factory.Value(3, id, 1),
-            Random.Shared.NextInt64(),
+            id,
             version: 3);
         
         var slices = buffer.AppendObjects([t1, t2, t3]);
@@ -49,29 +43,6 @@ static class Program
         Console.WriteLine(buffer.ExtractObject(slices[1]));
         Console.WriteLine(buffer.ExtractObject(slices[2]));
         Console.WriteLine(buffer.ExtractObject(mergedSlice));
-
-        // var (s1, s2, s3) = ("<@1-1 1:2>", "<@2-2 1:1:3>", "<@3-3 1:4:3>");
-        // // var (s1, s2) = ("[1, 2]", "[1, 1, 3]");
-        // // var (sb1, sb2, sb3) = (Marshal.StringToHGlobalAnsi(s1), Marshal.StringToHGlobalAnsi(s2), Marshal.StringToHGlobalAnsi(s3));
-        // // var (sl1, sl2, sl3) = (sb1 + s1.Length, sb2 + s2.Length, sb3 + s3.Length);
-        //
-        // var buffer = new RdxBuffer(1024);
-        //
-        // var sliceOfSlices = RdxSlice.OfSlices([buffer.AppendObject(s1), buffer.AppendObject(s2), buffer.AppendObject(s3)]);
-        // // RdxExportedFunctions.RDXJdrainExport(buffer.FreeSlice.Borders, [sb1, sl1]);
-        // // var eos1 = buffer.WrittenTo;
-        // // RdxExportedFunctions.RDXJdrainExport(buffer.FreeSlice.Borders, [sb2, sl2]);
-        // // var eos2 = buffer.WrittenTo;
-        // // RdxExportedFunctions.RDXJdrainExport(buffer.FreeSlice.Borders, [sb3, sl3]);
-        // // var eos3 = buffer.WrittenTo;
-        //
-        // // var sliceOfSlices = new RdxSlice(new RdxSlice(buffer.WrittenFrom, eos1), new RdxSlice(eos1, eos2), new RdxSlice(eos2, eos3));
-        // // var slices = new[] { buffer.WrittenFrom, eos1, eos1, eos2 };
-        // // var slicesPtr = GCHandle.Alloc(slices, GCHandleType.Pinned).AddrOfPinnedObject();
-        // // var sliceOfSlices = new[] { slicesPtr, slicesPtr + 32 };
-        // RdxExportedFunctions.RDXYExport(buffer.FreeSlice.Borders, sliceOfSlices.slice.Borders);
-        // RdxExportedFunctions.RDXJfeedExport(buffer.FreeSlice.Borders, [buffer.buffer, buffer.buffer + buffer.WrittenLength]);
-        // Console.WriteLine(Marshal.PtrToStringAnsi(buffer.buffer, buffer.WrittenLength));
     }
 
     public static unsafe void Main()
