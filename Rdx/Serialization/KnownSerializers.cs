b7848d7f050a8b199bd184a3cf88a827340fac52
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2025-01-07 15:03:05 +0500

    feat: remove unused classes

--- Rdx/Serialization/KnownSerializers.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Concurrent;
-using Rdx.Serialization.Attributes;
-
-namespace Rdx.Serialization;
-
-public static class KnownSerializers
-{
-    public static readonly ConcurrentDictionary<Type, RdxSerializerAttribute> Values = new();
-}
